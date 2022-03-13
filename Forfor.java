9719cee9eed89c950358ff48230a6a471e45ca68
jojorunojobana <jojorunojobana@gmail.com>
2022-03-14 07:54:28 +0900

    二重for文

---
+++ Forfor.java
@@ -0,0 +1,11 @@
+class Forfor {
+	public static void main (String[] args) {
+    String display ="";
+	for(int i = 0; i<9; i++){
+        for(int j = 0; j<=9; j++){
+            display = display + i + j + " ";
+        }
+        System.out.println(display);
+    }
+    }	
+}
