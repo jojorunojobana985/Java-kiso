253e5fcdd0aae691f3a387f31642b1308acc44b6
jojorunojobana <jojorunojobana@gmail.com>
2022-03-16 21:36:47 +0900

    if文

---
+++ Option.java
@@ -0,0 +1,19 @@
+public class Option{
+    public static void main(String[]args){
+        int testScore = Integer.parseInt(args[0]);
+        if(!(testScore >=0 && testScore <=100)){
+            System.out.println("不正な点数です");
+        }else if(testScore >=0 && testScore <=59){
+            System.out.println("赤点です");
+        }else if(testScore >=60 && testScore <=79){
+            System.out.println("普通です");
+        }else if(testScore >=80 && testScore <=100){
+            System.out.println("優秀です");
+        }
+        if(testScore == 100){
+            System.out.println("最高や！！素晴らしい！！！");
+        }else if(testScore == 0){
+            System.out.println("底が見えたな！！次があるぞ！！");
+        }
+    }    
+}   
